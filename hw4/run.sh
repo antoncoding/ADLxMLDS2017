4645b7b8e4e379e1f9e18b48e158e295ffa6c378
Anton Cheng <b03705027@ntu.edu.tw>
2018-01-07 12:29:18 +0800

    py version

--- hw4/run.sh
+++ hw4/run.sh
@@ -1,3 +1,3 @@
 git clone https://gitlab.com/antoncoding/gan-models.git
-python3 generate_preprocess.py --text_file $1
-python3 generate.py
+python3.5 generate_preprocess.py --text_file $1
+python3.5 generate.py
