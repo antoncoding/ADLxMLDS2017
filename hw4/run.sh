089a41f4df573117ae65ab36ee68e21bf54c2db1
Anton <b03705027@ntu.edu.tw>
2018-01-07 01:16:07 -0500

    test side no noise

--- hw4/run.sh
+++ hw4/run.sh
@@ -1,3 +1,3 @@
-git clone https://gitlab.com/antoncoding/gan-models.git
+#git clone https://gitlab.com/antoncoding/gan-models.git
 python3.5 generate_preprocess.py --text_file $1
 python3.5 generate.py
